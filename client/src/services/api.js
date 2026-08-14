45919659778df08b20da2e9052e4b0ec7d4f44dc
PARTHA-BATABYAL-official <parthabatabyal.03.30.2004@gmail.com>
2026-08-14 21:38:07 +0530

    cl api updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
-import env from 'dotenv';
-
-env.config();
 
 const API = axios.create({
-  baseURL: process.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || '/api',
   headers: {
     'Content-Type': 'application/json'
   }
@@ -14,9 +11,11 @@ const API = axios.create({
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('auralink_token');
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => Promise.reject(error)
